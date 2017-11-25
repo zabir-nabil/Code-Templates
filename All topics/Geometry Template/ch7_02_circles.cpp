2591a4a5b1d3ac7d9e499f8b72dd789c2c1cc7e9
zabir-nabil <zabiralnazi@codeassign.com>
2017-11-25 19:49:46 +0600

    segT

--- All topics/Geometry Template/ch7_02_circles.cpp
+++ All topics/Geometry Template/ch7_02_circles.cpp
@@ -24,7 +24,7 @@ int insideCircle(point_i p, point_i c, int r) { // all integer version
   return Euc < rSq ? 0 : Euc == rSq ? 1 : 2; } //inside/border/outside
 
 bool circle2PtsRad(point p1, point p2, double r, point &c) {
-  double d2 = (p1.x - p2.x) * (p1.x - p2.x) + 
+  double d2 = (p1.x - p2.x) * (p1.x - p2.x) +
               (p1.y - p2.y) * (p1.y - p2.y);
   double det = r * r / d2 - 0.25;
   if (det < 0.0) return false;
@@ -32,6 +32,13 @@ bool circle2PtsRad(point p1, point p2, double r, point &c) {
   c.x = (p1.x + p2.x) * 0.5 + (p1.y - p2.y) * h;
   c.y = (p1.y + p2.y) * 0.5 + (p2.x - p1.x) * h;
   return true; }         // to get the other center, reverse p1 and p2
+
+//intersection points of circle
+//Reference :http://www.ambrsoft.com/TrigoCalc/Circles2/circle2intersection/CircleCircleIntersection.htm
+ pair<point>intersectPts()
+ {
+
+ }
 
 int main() {
   // circle equation, inside, border, outside
